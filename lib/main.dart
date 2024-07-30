647b6bd84604dcfa903edeeef4aaea0cdeb1862b
gitcrusher <aayushguptasoni@gmail.com>
2024-07-30 19:32:22 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: "Flutter Welcome's Aayush"),
@@ -36,148 +36,231 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body:
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      // body:
 
+//Use of ScrollView in flutter ---------------------------->
 
+      // Padding(
+      //   padding: const EdgeInsets.all(11),
+      //   child: SingleChildScrollView(
+      //     child: Column(
+      //       children: [
+      //         Padding(
+      //           padding: const EdgeInsets.only(bottom: 11),
+      //           child: SingleChildScrollView(
+      //             scrollDirection: Axis.horizontal,
+      //             child: Row(
+      //               children: [
+      //                 Container(
+      //                   margin: EdgeInsets.only(right: 11),
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.teal,
+      //                 ),
+      //                 Container(
+      //                   margin: EdgeInsets.only(right: 11),
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.indigo,
+      //                 ),
+      //                 Container(
+      //                   margin: EdgeInsets.only(right: 11),
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.lime,
+      //                 ),
+      //                 Container(
+      //                   margin: EdgeInsets.only(right: 11),
+      //                   height: 200,
+      //                   width: 200,
+      //                   color: Colors.cyan,
+      //                 ),
+      //               ],
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.blue,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.yellow,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.red,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.purple,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.pink,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.grey,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.black,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.deepOrange,
+      //         ),
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 11),
+      //           height: 200,
+      //           color: Colors.blueGrey,
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // )
 
+//USe of Inkwell() function -------------------------->
 
+      //      Center(
+      //   child: InkWell(
+      //     onTap: () {
+      //       print("tapped on Container");
+      //     },
+      //     onLongPress: () {
+      //       print("longtapped on Container");
+      //     },
+      //     onDoubleTap: () {
+      //       print("doubletapped on Container");
+      //     },
+      //     child: Container(
+      //       width: 200,
+      //       height: 200,
+      //       color: Colors.amber,
+      //       child: Center(
+      //         child: InkWell(
+      //           onTap: () {
+      //             print("tapped on Text!");
+      //           },
+      //           child: Text(
+      //             "click here",
+      //             style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // )
 
+//Rows And Columns Explaination  ------------------------------>
 
-//USe of Inkwell() function.
+      //       Container(
+      //   height: 800,
+      //   width: 500,
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.start,
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     children: [
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         crossAxisAlignment: CrossAxisAlignment.center,
+      //         children: [
+      //           Text(
+      //             'A',
+      //             style: TextStyle(fontSize: 20),
+      //           ),
+      //           Text(
+      //             'B',
+      //             style: TextStyle(fontSize: 20),
+      //           ),
+      //           Text(
+      //             'C',
+      //             style: TextStyle(fontSize: 20),
+      //           ),
+      //           Text(
+      //             'D',
+      //             style: TextStyle(fontSize: 20),
+      //           ),
+      //           Text(
+      //             'E',
+      //             style: TextStyle(fontSize: 20),
+      //           ),
+      //           Text(
+      //             'F',
+      //             style: TextStyle(fontSize: 20),
+      //           ),
+      //         ],
+      //       ),
+      //       Text(
+      //         'A',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       Text(
+      //         'B',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       Text(
+      //         'C',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       Text(
+      //         'D',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       Text(
+      //         'E',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       Text(
+      //         'F',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       ElevatedButton(onPressed: () {}, child: Text('click me'))
+      //     ],
+      //   ),
+      // )
 
+//images inserting in docs ---------------------->
 
+      // Center(
+      //   child: Container(
+      //       width: 800,
+      //       height: 500,
+      //       child: Image.asset(
+      //           'assets/images/tuqa-nabi-uhnZZUaTIOs-unsplash.jpg')),
+      // )
 
-        //      Center(
-        //   child: InkWell(
-        //     onTap: () {
-        //       print("tapped on Container");
-        //     },
-        //     onLongPress: () {
-        //       print("longtapped on Container");
-        //     },
-        //     onDoubleTap: () {
-        //       print("doubletapped on Container");
-        //     },
-        //     child: Container(
-        //       width: 200,
-        //       height: 200,
-        //       color: Colors.amber,
-        //       child: Center(
-        //         child: InkWell(
-        //           onTap: () {
-        //             print("tapped on Text!");
-        //           },
-        //           child: Text(
-        //             "click here",
-        //             style: TextStyle(fontSize: 21, fontWeight: FontWeight.w700),
-        //           ),
-        //         ),
-        //       ),
-        //     ),
-        //   ),
-        // )
+//child nesting --------------------------------->
 
-//Rows And Columns Explaination
-
-        //       Container(
-        //   height: 800,
-        //   width: 500,
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.start,
-        //     crossAxisAlignment: CrossAxisAlignment.start,
-        //     children: [
-        //       Row(
-        //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //         crossAxisAlignment: CrossAxisAlignment.center,
-        //         children: [
-        //           Text(
-        //             'A',
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //           Text(
-        //             'B',
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //           Text(
-        //             'C',
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //           Text(
-        //             'D',
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //           Text(
-        //             'E',
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //           Text(
-        //             'F',
-        //             style: TextStyle(fontSize: 20),
-        //           ),
-        //         ],
-        //       ),
-        //       Text(
-        //         'A',
-        //         style: TextStyle(fontSize: 20),
-        //       ),
-        //       Text(
-        //         'B',
-        //         style: TextStyle(fontSize: 20),
-        //       ),
-        //       Text(
-        //         'C',
-        //         style: TextStyle(fontSize: 20),
-        //       ),
-        //       Text(
-        //         'D',
-        //         style: TextStyle(fontSize: 20),
-        //       ),
-        //       Text(
-        //         'E',
-        //         style: TextStyle(fontSize: 20),
-        //       ),
-        //       Text(
-        //         'F',
-        //         style: TextStyle(fontSize: 20),
-        //       ),
-        //       ElevatedButton(onPressed: () {}, child: Text('click me'))
-        //     ],
-        //   ),
-        // )
-
-//images inserting in docs
-
-        // Center(
-        //   child: Container(
-        //       width: 800,
-        //       height: 500,
-        //       child: Image.asset(
-        //           'assets/images/tuqa-nabi-uhnZZUaTIOs-unsplash.jpg')),
-        // )
-
-//child nesting.
-
-        //Center(
-        //   child: Container(
-        //     width: 200,
-        //     height: 100,
-        //     color: Colors.black,
-        //     child: Center(
-        //       child: Text(
-        //         "flutter oops",
-        //         style: TextStyle(
-        //             fontSize: 15,
-        //             color: Colors.white,
-        //             fontWeight: FontWeight.bold),
-        //       ),/
-        //     ),
-        //   ),
-        // )
-
-        );
+      //Center(
+      //   child: Container(
+      //     width: 200,
+      //     height: 100,
+      //     color: Colors.black,
+      //     child: Center(
+      //       child: Text(
+      //         "flutter oops",
+      //         style: TextStyle(
+      //             fontSize: 15,
+      //             color: Colors.white,
+      //             fontWeight: FontWeight.bold),
+      //       ),/
+      //     ),
+      //   ),
+      // )
+    );
   }
 }
